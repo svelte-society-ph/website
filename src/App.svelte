44890b6dd1e4008f7ae42d8e44d1894044cad457
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-22 16:57:43 +0800

    FEAT: Add App.svelte file

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Container, Col, Button, InputGroup, Input, InputGroupAddon, Form, ListGroup, ListGroupItem } from "sveltestrap";
+</script>
+
+<style>
+
+</style>
+
+<Container class="container">
+  <h1>WELCOME to Svelte Society PH</h1>
+</Container>

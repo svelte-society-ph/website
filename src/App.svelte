1bf1ea5dba3b28c55e9ce70b47f92fd4a13ca88d
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-22 17:19:38 +0800

    FEAT: Center the logo and text

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,9 @@
 
 </style>
 
-<Container class="container">
-  <h1>WELCOME to Svelte Society PH</h1>
+<Container class="d-flex container text-center h-100 align-items-center justify-content-center">
+  <div>
+    <img class="img-responsive mb-5" src="./svelte-ph-logo.png" alt="Svelte Society PH" height="400px" />
+    <h1>WELCOME to Svelte Society PH</h1>
+  </div>
 </Container>
